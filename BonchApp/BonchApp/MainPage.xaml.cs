e4b96305471d3ab385a5d6b11cf3111a0b213396
danya140 <daniilhacker@gmail.com>
2017-08-05 21:35:35 +0300

    Init project

---
+++ BonchApp/BonchApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BonchApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Button1.BackgroundColor = new Color(256, 256, 256, 0.7);
+            Entry1.PlaceholderColor = Color.LightGray;
+            Entry2.PlaceholderColor = Color.LightGray;
+		}
+	}
+}
