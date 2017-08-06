99dc444dcc0c9449e7d6be07b1b1b822b4185cfd
danya140 <daniilhacker@gmail.com>
2017-08-06 20:01:33 +0300

    Create NavBar and NavMenu design

--- BonchApp/BonchApp.Android/MainActivity.cs
+++ BonchApp/BonchApp.Android/MainActivity.cs
@@ -10,11 +10,13 @@ using Android.OS;
 namespace BonchApp.Droid
 {
 	[Activity (Label = "BonchApp", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
-			TabLayoutResource = Resource.Layout.Tabbar;
+            
+            TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar; 
 
 			base.OnCreate (bundle);
